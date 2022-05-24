11435d856a91ce817126af625601a45d72c7b8ef
Rodrigo Rodrigues <0rr@outlook.com.br>
2022-05-24 12:33:29 -0300

    solved connection ssh

--- vmmanager.sh
+++ vmmanager.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#
-#Initial Param
-#
-vm=201
-node= egrep -o "\"$vm\": { .*" /etc/pve/.vmlist|awk '{print $4}'| egrep -o -i "[a-z|0-9]*"
-#Connect to node...
-for vm in node; do 
-    ssh $node ls /etc/pve/
-done
+
+#List Node
+node=$(egrep -o "\"$id_vm\": { .*" /etc/pve/.vmlist|awk '{print $4}'| egrep -o -i "[a-z|0-9]*")
+
+#Connect to Node PVE
+ssh $node qm start 201
