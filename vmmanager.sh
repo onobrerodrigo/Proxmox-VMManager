b3b56f34e400615b8d85745eae5837de43764c8d
Rodrigo Rodrigues <0rr@outlook.com.br>
2022-05-24 16:25:50 -0300

    Organize comments

--- vmmanager.sh
+++ vmmanager.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#External parameter
+id_vm=201
 
-#List Node
+#Node list
 node=$(egrep -o "\"$id_vm\": { .*" /etc/pve/.vmlist|awk '{print $4}'| egrep -o -i "[a-z|0-9]*")
 
 #Connect to Node PVE
-ssh $node qm start 201
+ssh $node qm shutdown $id_vm
